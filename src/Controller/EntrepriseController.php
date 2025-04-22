b22793c82beb4e686330a296764e42278a5034b4
Ivan Veltz <ivan.veltz@live.fr>
2025-04-22 11:32:09 +0200

    test affichage entreprises

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Entreprise;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EntrepriseController extends AbstractController
 {
     /**
      * @Route("/entreprise", name="app_entreprise")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
         return $this->render('entreprise/index.html.twig', [
-            'controller_name' => 'EntrepriseController',
+            'entreprises' => $entreprises,
         ]);
     }
 }
